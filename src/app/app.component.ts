6014be75875c6381f64eb893291750f9c37b7239
deepan <deepanappu1999@gmail.com>
2025-05-10 16:20:03 +0530

    adding parent to main comp

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { ParentComponent } from './components/parent/parent.component';
+import { ContentComponent } from './components/content/content.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet,ParentComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
